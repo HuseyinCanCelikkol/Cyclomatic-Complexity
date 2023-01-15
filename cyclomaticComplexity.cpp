04748c47d54ffe004407226301a1f8e1dece07b2
HuseyinCanCelikkol <48149182+HuseyinCanCelikkol@users.noreply.github.com>
2023-01-16 02:36:44 +0300

    Made it to Calculate complexity
    It simply counts some arguments in code which explained in KEYS string array.

--- cyclomaticComplexity.cpp
+++ cyclomaticComplexity.cpp
@@ -5,31 +5,60 @@
 #include <ostream>
 #include <algorithm>
 #include <cctype>
-#define KEY_VALUE 50
 
-
+#define KEY_VALUE 6
+#define STRING_SIZE 32
+bool FoundKey = false;
 using namespace std;
 ifstream Code;
 string rowOfCode;
 string KEYS[KEY_VALUE] = {"if(","while(","for(","?", "&&","||"};
-
+int cyclomaticComplexityVal = 0;
 int main(){
-Code.open("temp.txt");
-while(!Code.eof()){
-    getline(Code, rowOfCode, '\n'); // use ' ' as separator, default is '\n' (newline). Now name is "John".
-    rowOfCode.erase(remove_if(rowOfCode.begin(), rowOfCode.end(), ::isspace), rowOfCode.end());
-cout << rowOfCode << endl; 
 
-   /* for(int i = 0;i<rowOfCode.length();i++){
-        for(int j = 0;j<sizeof(KEYS);j++){
-            if(KEYS[j][0] == rowOfCode[i]){
+Code.open("temp.txt"); // open the txt
+
+while(!Code.eof()){
+    getline(Code, rowOfCode, '\n'); // dosyadaki her bir line ı al
+    rowOfCode.erase(remove_if(rowOfCode.begin(), rowOfCode.end(), ::isspace), rowOfCode.end()); // satırdaki boşlukları sil
+    //cout << rowOfCode << endl; 
+
+   for(int rowIterator = 0;rowIterator<rowOfCode.length();rowIterator++){ // kelimenin i. harfi
+        for(int keysIterator = 0;keysIterator<sizeof(KEYS)/STRING_SIZE;keysIterator++){
+            if(FoundKey == true){ // eğerki halihazırda bir yapı bulduysak (kelimeyi karşılaştırmaya devam etmesin)
+                FoundKey = false;
+                break;
+                } 
+            if(KEYS[keysIterator][0] == rowOfCode[rowIterator]){
+                if(KEYS[keysIterator][0] == KEYS[3][0]){ // soru işaretini tanımıyor tek karakter olduğundan. o yüzden böyle bir koşul yazdım
+                    cyclomaticComplexityVal++;
+                    break;
+                }
                 
+            int rowIteratorForKeyTest = rowIterator+1; 
+            for(int letterOfKeyIterator = 1;letterOfKeyIterator<KEYS[keysIterator].length();letterOfKeyIterator++){ 
+                if(KEYS[keysIterator][letterOfKeyIterator] == rowOfCode[rowIteratorForKeyTest]){// keys in keysIterator. elemanının k. harfi de rowOfCode'nin devamıyla eşleşiyorsa
+                    
+                    if(letterOfKeyIterator == KEYS[keysIterator].length()-1 && KEYS[keysIterator].at(KEYS[keysIterator].length()-1) == rowOfCode[rowIteratorForKeyTest]){ // artık kelimenin son harfi de eşleşmeye devam ediyorsa
+                        cyclomaticComplexityVal++;
+                        rowIterator = rowIteratorForKeyTest+1;
+                        FoundKey = true;
+                        break;
+                    }
+                    rowIteratorForKeyTest++;
+
+                    }
+                    else{break;
+                    }
+                }
             }
+            
         }
-    }*/
+    }
 }
 
-  
+cyclomaticComplexityVal = cyclomaticComplexityVal+1; // formülden ötürü
+cout << cyclomaticComplexityVal << endl;
 
 Code.close(); 
 }
